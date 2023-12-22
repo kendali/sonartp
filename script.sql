c3ab0a02285a3f2e07ea7af8256050c8540d671b
kendali <bouchaibkendali@gmail.com>
2023-12-22 11:11:44 +0100

    modify database

--- script.sql
+++ script.sql
@@ -1,9 +1,8 @@
-Create database address_book;
+CREATE DATABASE address_book;
 
-create table
-    customers (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name varchar(40),
-        email varchar(80),
-        mobile varchar(40)
-    );
+CREATE TABLE customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40),
+    email VARCHAR(80),
+    mobile VARCHAR(40)
+);
